d2ae0122aa9aa68457f8a6144dd19d0d035effab
petss1 <petss1988@gmail.com>
2025-05-30 11:10:20 -0600

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import './App.css';
-import Header from './component/Header';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
 export default App;
+// образец
+// import './index.css'
+// import './App.css'
+// import TariffCard from './components/TariffCard'
+
+// function App() {
+
+//   return (
+//     <div className="tariffs_container">
+//   <TariffCard price={300} title="Безлимитный 300" description="до 10 Мбит/сек" colorClass="yellow" />
+//   <TariffCard price={500} title="Безлимитный 500" description="до 50 Мбит/сек" colorClass="green" />
+//   <TariffCard price={700} title="Безлимитный 700" description="до 100 Мбит/сек" colorClass="red" highlighted />
+
+//     </div>
+//   )
+// }
+
+// export default App
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,19 @@
+// const Card = (props) => {
+//     const cardClasses = `tariffCard ${props.colorClass} ${props.highlighted ? 'highlighted' : ''}`;
+
+//     return (
+//         <div className={cardClasses}>
+//             <div className="tariffСard__body">
+//                 <h4 className="tariffСard__title">{props.title}</h4>
+//                 <p className="tariffСard__text">{props.description}</p>
+//             </div>
+//             <div className="tariffСard__footer">
+//                 <span className="tariffСard__price">{props.price} руб/мес</span>
+//                 <button className="tariffСard__on">Подключить</button>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Card;
+// образец
