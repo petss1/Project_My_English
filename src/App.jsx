e8ab8f8f3b08673f536a19983acd90a7b74f3ea0
petss1 <petss1988@gmail.com>
2025-06-22 10:12:04 -0600

    feat: add 404 error page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from './components/Footer/Footer';
 import WordTable from './components/WordTable/WordTable';
 import { Routes, Route } from "react-router-dom";
 import CardsPage from "./pages/CardsPage";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<WordTable />} />
           <Route path="/cards" element={<CardsPage />} />
+          <Route path="*" element={<NotFound />} /> 
         </Routes>
       </main>
       <Footer />
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound () {
+    return (
+        <div style = {{textAlign: "center", marginTop: "50px"}}>
+            <h1> Ошибка 404. Страница не найдена</h1>
+            <p>Упс! Такой страницы не существует</p>
+        </div>
+    );
+}
+
+export default NotFound;
